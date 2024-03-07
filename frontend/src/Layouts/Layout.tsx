5c33f6fc037baa32f0a931f04b769e74b60f3506
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-07 16:21:38 +0300

    Импортирован Hero

--- frontend/src/Layouts/Layout.tsx
+++ frontend/src/Layouts/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //components
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 type TProps = {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ const Layout: React.FC<TProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
   );
