20378b82289010a34a29a45c1679d29d920ec6d4
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-19 13:40:06 +0300

    Импортирован myUserRoute

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,9 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 
+//routes
+import myUserRoute from "../routes/myUserRoutes";
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to database");
 });
@@ -11,9 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello" });
-});
+// app.get("/test", async (req: Request, res: Response) => {
+//   res.json({ message: "hello" });
+// });
+
+app.use("/api/my/user", myUserRoute);
 
 app.listen(8000, () => {
   console.log("Server starts on localhost:8000");
