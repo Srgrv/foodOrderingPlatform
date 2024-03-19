853a6b23c7ab2c0e50fcf5dd4460fabcf06adfaa
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-19 13:09:02 +0300

    Внедрена библиотека auth0 для авторицации

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 //components
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav: React.FC = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
-    <Button
-      variant="ghost"
-      className="font-bold hover:text-orange-500 hover:bg-white"
-    >
-      Log In
-    </Button>
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated ? (
+        <UsernameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white"
+          onClick={async () => loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
+    </span>
   );
 };
 
