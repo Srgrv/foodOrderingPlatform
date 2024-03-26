bec883fe217a8fe64a9f43c6a52c3f212fbf7cdf
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-26 22:04:39 +0300

    Создан router для обновления данных пользователя

--- backend/src/routes/myUserRoutes.ts
+++ backend/src/routes/myUserRoutes.ts
@@ -4,11 +4,19 @@ import express from "express";
 import MyUserController from "../controllers/MyUserController";
 
 //middleware
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // /api/my/user
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
