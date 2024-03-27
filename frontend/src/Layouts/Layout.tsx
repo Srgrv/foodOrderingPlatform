bc9626bb705273c8076df5c45950b0267727cacf
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-27 14:57:53 +0300

    Добавлен дополнительное свойство для отображения либо для скрытия компонента Hero

--- frontend/src/Layouts/Layout.tsx
+++ frontend/src/Layouts/Layout.tsx
@@ -7,13 +7,14 @@ import Footer from "@/components/Footer";
 
 type TProps = {
   children: React.ReactNode;
+  showHero?: boolean;
 };
 
-const Layout: React.FC<TProps> = ({ children }) => {
+const Layout: React.FC<TProps> = ({ children, showHero = false }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero />
+      {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
