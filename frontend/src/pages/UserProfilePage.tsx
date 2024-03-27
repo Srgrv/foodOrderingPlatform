41421c963fd3bb7c0fcabe58f407c338a9f0ecfc
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-27 15:12:59 +0300

    Создана страница UserProfilePage

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//components
+import { UserProfileForm } from "@/forms/user-profile-form/UserProfileForm";
+
+const UserProfilePage: React.FC = () => {
+  return <UserProfileForm />;
+};
+
+export default UserProfilePage;
