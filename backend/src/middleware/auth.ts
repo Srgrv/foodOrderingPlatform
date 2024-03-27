c73d47e15d7cd3a9a97fe021ff994be9fb3ada11
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-27 23:10:16 +0300

    Исправил комментарии

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -18,16 +18,17 @@ declare global {
   }
 }
 
-// Таким образом, после этого объявления, при использовании объекта запроса (Request) в вашем Express.js приложении, вы можете обращаться к req.userId и req.auth0Id, чтобы получить доступ к соответствующим идентификаторам пользователя.
+// используется для проверки действительности и подлинности JWT на основе заданных параметров. Он не извлекает информацию о пользователе, а только проверяет, соответствует ли предоставленный JWT заданным параметрам
 
-// функция для аутентификации пользователя, здесь происходит валидация на auth0
+// когда приходит запрос, содержащий JWT, middleware jwtCheck проверяет, соответствует ли предоставленный JWT этим параметрам.
+// если JWT проходит проверку, то управление передается следующему middleware или обработчику маршрута. Если нет, то возвращается код состояния 401 (Unauthorized), и запрос дальше не обрабатывается.
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
 });
 
-// функция для авторизации пользователя, здесь происходит декодирования jwt токена, так как auth0 возвращает jwt token на основе логина и пароля, нам нужно декодировать используя jsonwebtoken библиотеку
+// используется для извлечения и проверки JWT, а также для аутентификации пользователей и добавления информации о пользователе к запросу
 export const jwtParse = async (
   req: Request,
   res: Response,
@@ -36,7 +37,8 @@ export const jwtParse = async (
   // достаем токет хранящийся в headers в виде Bearer adjf;adjf;ajdfa;dfj
   const { authorization } = req.headers;
 
-  // проверяем на отсутствие и отсутствие начала токена на Bearer
+  // проверяется, передается ли в запросе заголовок Authorization, и начинается ли токен с "Bearer ".
+  // если заголовок Authorization отсутствует или не начинается с "Bearer ", то возвращается код состояния 401 (Unauthorized).
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return res.sendStatus(401);
   }
@@ -45,22 +47,24 @@ export const jwtParse = async (
   const token = authorization.split(" ")[1];
 
   try {
-    // декодируем токен
+    // декодируется с использованием библиотеки jwt.
     const decoded = jwt.decode(token) as jwt.JwtPayload;
-    // достаем из декодированного токена sub и присваиваем его переменной auth0Id
-    const auth0Id = decoded.sub;
-    // находим пользователя по auth0Id
-    const user = await User.findOne({ auth0Id });
 
+    // затем из декодированного токена извлекается идентификатор пользователя (auth0Id)
+    const auth0Id = decoded.sub;
+    // на основе этого идентификатора auth0 выполняется поиск пользователя в базе данных.
+    const user = await User.findOne({ auth0Id });
+    console.log(user);
+    // если пользователь не найден, возвращается код состояния 401 (Unauthorized)
     if (!user) {
       return res.sendStatus(401);
     }
 
-    // добавляем новые свойства запросу в виду auth0Id и userId
+    // если пользователь найден, то его идентификатор (auth0Id) и идентификатор MongoDB (userId) добавляются к объекту запроса (req) для последующего использования другими middleware и обработчиками маршрутов.
     req.auth0Id = auth0Id as string;
     req.userId = user._id.toString();
 
-    // передаем управление следующему middleware
+    // затем управление передается следующему middleware или обработчику маршрута с помощью функции next()
     next();
   } catch (error) {
     return res.sendStatus(401);
