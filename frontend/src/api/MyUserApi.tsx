6e776ea5cdf2650bce9fb04a1d7c0c3fbe89206f
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-28 19:00:26 +0300

    Создан fetch запрос для получения пользователя

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,6 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
+
+//components
 import { toast } from "sonner";
+
+//types
+import { User } from "@/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -56,7 +61,6 @@ export const useUpdateMyUser = () => {
   const { getAccessTokenSilently } = useAuth0();
 
   const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
-    console.log(API_BASE_URL);
     const accessToken = await getAccessTokenSilently();
 
     const response = await fetch(`${API_BASE_URL}/api/my/user`, {
@@ -100,4 +104,36 @@ export const useUpdateMyUser = () => {
     error,
     reset,
   };
+};
+
+export const useGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const getMyUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      console.log(response);
+      throw new Error("Failed to get user");
+    }
+    return response.json();
+  };
+
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return { currentUser, isLoading };
 };
