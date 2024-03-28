c719b7e325b03b55a9e8cfec8db25ceac109d421
Sergey Georgiev <sgrgv_art@mail.ru>
2024-03-28 19:10:28 +0300

    Импортирова хук useGetMyUser и использован для рендера и передачи свойств в качестве пропсов

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -2,11 +2,27 @@ import React from "react";
 
 //components
 import { UserProfileForm } from "@/forms/user-profile-form/UserProfileForm";
-import { useUpdateMyUser } from "@/api/MyUserApi";
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 
 const UserProfilePage: React.FC = () => {
-  const { updateUser, isLoading } = useUpdateMyUser();
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+
+  return (
+    <UserProfileForm
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+      currentUser={currentUser}
+    />
+  );
 };
 
 export default UserProfilePage;
